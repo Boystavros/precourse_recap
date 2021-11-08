b32879d740076af65f4575ed6c859fa6ba9297d7
Sacha Ponniah <sponniah812@gmail.com>
2021-11-08 17:18:36 +0000

    Add python code

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+my_name = "sacha"
+my_dogs_name = "WILBUR"
+my_weight = 93
+my_dogs_weight = 11
+
+my_name.capitalize()
+my_dogs_name.lower()
+dogs_to_people_ratio = round(my_weight / my_dogs_weight, 2)
+combined_weight = my_weight + my_dogs_weight
